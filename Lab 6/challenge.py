bdfd11a38b52691ecd9f41d24a734ac2eb06908d
Fabian Vikman <fabianwikman@gmail.com>
2026-09-17 16:57:32 +0200

    work in progress on lab 6 challenge started.

---
+++ Lab 6/challenge.py
@@ -0,0 +1,20 @@
+#THIS IS NOT FINISHED YET, AS THIS IS AN EXTRA ASSIGNMENT.
+
+#Part 1
+players = [
+    {"name" : "  Rekklez ", "team" : "Fnatic", "country" : "SwEDen", "score": 0, "matches" : 0, "wins" : 24, "active" : False},
+    {"name" : "xPeke", "team" : "Fnatic", "country" : "spaIN", "score": 0, "matches" : 0, "wins" : 24, "active" : False},
+    {"name" : "Faker", "team" : "T1", "country" : "South koREa", "score": 0, "matches" : 0, "wins" : 101, "active" : True},
+    {"name" : "      TenZ   ", "team" : "T1", "country" : "Canada", "score": 0, "matches" : 0, "wins" : 75, "active" : True},
+    {"name" : "SugarZ3ro  ", "team" : "ZERO DIVISION", "country" : "Japan", "score": 0, "matches" : 0, "wins" : 0, "active" : True},
+    {"name" : "    SHAKA", "team" : "ZERO DIVISION", "country" : "JApan", "score": 0, "matches" : 0, "wins" : 58, "active" : True},
+    {"name" : "S1mple", "team" : "BC.Game Esports", "country" : "Ukraine", "score": 0, "matches" : 0, "wins" : 0, "active" : False},
+    {"name" : "Magisk", "team" : "BC.Game Esports", "country" : "DenmArk", "score": 0, "matches" : 0, "wins" : 0, "active" : True},
+    {"name" : "HooXi", "team" : "Astralis", "country" : "DENmark", "score": 0, "matches" : 0, "wins" : 0, "active" : True},
+    {"name" : " phzy  ", "team" : "Astralix", "country" : "Sweden", "score": 0, "matches" : 0, "wins" : 0, "active" : True},
+    {"name" : "Zer0", "team" : "Team Liquid", "country" : "AustRAlia", "score": 0, "matches" : 0, "wins" : 0, "active" : False},
+    {"name" : " Mendo", "team" : "Team Liquid", "country" : "sWEDEn", "score": 0, "matches" : 0, "wins" : 42, "active" : True},
+    {"name" : "Pistillo         ", "team" : "Team Liquid", "country" : "MeXIco", "score": 0, "matches" : 0, "wins" : 0, "active" : False},
+    {"name" : "Caps   ", "team" : "G2 Esports", "country" : "Denmark", "score": 0, "matches" : 0, "wins" : 37, "active" : True},
+    {"name" : "   HaNs SaMA", "team" : "G2 Esports", "country" : "France", "score": 0, "matches" : 0, "wins" : 0, "active" : True},
+]
