d7574d0e5e5a3ecf75624c274eaacfa77c4b6478
Fabian Vikman <fabianwikman@gmail.com>
2026-09-17 14:27:49 +0200

    one left in lab 6 part g.

--- Lab 6/part_g.py
+++ Lab 6/part_g.py
@@ -1,15 +1,81 @@
 #Part G
 
 # -----1-----
+multiple_nums = [
+    [2, 4, 6],
+    [8, 10, 12],
+    [14, 16, 18]
+]
 
+#Honestly took a while, because I misinterpreted what n and nums is seen as per iteration until I wrote it down on paper.
+single_nums = [n for nums in multiple_nums for n in nums]
 
-# -----1-----
+#Proof
+print(single_nums)
 
+# -----2-----
+#Presuming we want an input, like earlier assignments, before creating it.
+limit = int(input("give a number limit: "))
 
-# -----1-----
+multiplication_table = [
+    [j*i for j in range(1, i+1)]
+    for i in range(1, limit+1)
+]
 
+#Proof
+print(multiplication_table)
 
-# -----1-----
+#I think that the +1 in the ranges make it slightly less unreadable,
+#otherwise I think it would have been relatively understandable
+#(that the multiplication per number "i" never stretches beyond itself ("j" in relation to what "i" is)).
 
+#A bit tricky maybe in the start, but if you're already familiar with 
+#nested for loops, it might just look a bit backwards, and that's all.
+
+# -----3-----
+names = ["Jameson", "Aubrey", "Travis", "Mila", "Kumar", "Ishmael"]
+scores = [60, 75, 80, 65, 90, 50]
+
+passing_students = [
+    {"name" : name, "score" : score} 
+    for name, score in zip(names, scores) 
+    if score >= 70
+]
+
+#Proof
+print(passing_students)
+
+# -----4-----
+#first solve with loops, not loop comprehension
+more_scores = [50, 70, 90, 40, 55, 65, 75, 85, 95]
+
+#[With for loops]
+#1. If anyone got over 90 points (in this case, that is a high grade)
+def any_amazing_score(scores : list) -> bool:
+    for score in scores:
+        if score >= 90:
+            return True 
+    return False
+
+#2. If everyone passed the acceptable limit of 60 points
+def all_passing_scores(scores : list) -> bool:
+    for score in scores:
+        if not score >= 60:
+            return False
+    return True
+
+#Should give True and False
+print(f"Q1: {any_amazing_score(more_scores)}")
+print(f"Q2: {all_passing_scores(more_scores)}")
+
+#[With any() and all()]
+#1. If anyone got over 90 points (in this case, that is a high grade)
+has_amazing_score = any([score >= 90 for score in more_scores])
+#2. If everyone passed the acceptable limit of 60 points
+has_everyone_passed = all([score >= 60 for score in more_scores])
+
+#Also gives True and False
+print(f"Q1: {has_amazing_score}")
+print(f"Q2: {has_everyone_passed}")
 
 # -----5-----
