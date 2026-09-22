a7732aa527ea8bbe91e856ada7776fa04864afaa
Fabian Vikman <fabianwikman@gmail.com>
2026-09-22 12:01:45 +0200

    lab 7 started.

---
+++ Lab 7/part_a.py
@@ -0,0 +1,27 @@
+#Part A
+
+#-----1-----
+class Book:
+    def __init__(self, title, author, pages):
+        self.title = title
+        self.author = author
+        self.pages = pages
+        
+#WIP: Creating the four Book objects
+
+#-----2-----
+class Laptop:
+    def __init__(self, brand, model, ram_gb, price):
+        self.brand = brand
+        self.model = model
+        self.ram_gb = ram_gb
+        self.price = price
+
+#-----3-----
+
+
+#-----4-----
+
+
+#-----5-----
+
