b9205e9edd1e53952b779592875e9afd223f11ac
Fabian Vikman <fabianwikman@gmail.com>
2026-09-22 16:13:49 +0200

    doing the final part of a and b.

--- Lab 7/part_a_and_b.py
+++ Lab 7/part_a_and_b.py
@@ -1,15 +1,23 @@
-#Part A
+#These parts seem to go hand in hand, so...
 
-#-----1-----
+
+#Part A
+#1
 class Book:
-    def __init__(self, title, author, pages):
+    #4
+    def __init__(self, title="unknown", author, pages):
         self.title = title
         self.author = author
         self.pages = pages
-        
-#WIP: Creating the four Book objects
 
-#-----2-----
+    #1 (Part B)
+    def is_long(self) -> bool:
+        if self.pages > 300:
+            return True
+        return False
+
+
+#2
 class Laptop:
     def __init__(self, brand, model, ram_gb, price):
         self.brand = brand
@@ -17,11 +25,12 @@ class Laptop:
         self.ram_gb = ram_gb
         self.price = price
 
-#-----3-----
+#3
 
 
-#-----4-----
+#4
 
 
-#-----5-----
+#5
 
+#Part B
