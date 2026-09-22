96e22d432dcfdc3fa7c29145cad2901f0fe6c931
Fabian Vikman <fabianwikman@gmail.com>
2026-09-22 13:46:40 +0200

    lab 7 part f valueerror fixed.

--- Lab 7/part_f.py
+++ Lab 7/part_f.py
@@ -8,16 +8,13 @@ class Student:
         self.name = name
         self.score = score
 
-        #3
-        def get_status(self) -> str:
-            #8
-            if self.score < 0:
-                raise ValueError("Given score is under 0, which should not be possible.")
-            
-            if self.score >= 70:
-                return "PASS"
-            else:
-                return "FAIL"
+    #3
+    def get_passing(self) -> str:
+        #8
+        if self.score >= 70:
+            return "PASS"
+        else:
+            return "FAIL"
 
 #4
 class Teacher:
@@ -33,6 +30,9 @@ class Course:
 
     #6
     def add_student(self, new_student : Student) -> None:
+        if new_student.score < 0:
+            raise ValueError("Given score is under 0, which should not be possible.")
+        
         self.students.append(new_student)
 
     #6
@@ -44,27 +44,33 @@ class Course:
 
         passing_students = []
         for student in self.students:
-            if student.get_status() == "PASS":
+            if student.get_passing() == "PASS":
                 passing_students.append(student)
         return passing_students
 
 #9
 student1 = Student("Jüri", 50)
-student2 = Student("Strangey", -5)
+student2 = Student("Mawa", 65)
 student3 = Student("Perkka", 90)
-student4 = Student("Kasane", 100)
-student5 = Student("Mawa", 65)
+student4 = Student("Kasane", 99)
+student5 = Student("Strangey", -5)
 
 teacher = Teacher("Marissa")
 
 course = Course("OOP", "Robotzi", [student1, student2, student3, student4])
 
-course.add_student(student5)
+#Will give a ValueError
+#course.add_student(student5)
+
+#Will work fine
+student6 = Student("Excellenzi", 100)
+course.add_student(student6)
 
 print(course.count_students())
 
 print(course.get_passing_students())
 
-#måste göra status-saken också
+print(course.students[1].get_passing())
+print(course.students[2].get_passing())
 
 #10: Summary print
