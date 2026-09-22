a5f90f72675794c1fff72285f7da1f84a0c0b6df
Fabian Vikman <fabianwikman@gmail.com>
2026-09-22 15:24:10 +0200

    part d should be fine, let's fix the rest of e then.

--- Lab 7/part_d_and_e.py
+++ Lab 7/part_d_and_e.py
@@ -2,17 +2,42 @@
 
 #Part D 
 
+#Assuming that we add the class to even begin with.
+class Student:
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+
+    #4
+    def get_status(self):
+        if self.score >= 80:
+            return "PASS"
+        return "FAIL"
+
 #1
+s1 = Student("John", 60)
+s2 = Student("Martin", 95)
+s3 = Student("Xavier", 85)
+s4 = Student("Jessica", 40)
+s5 = Student("Tanya", 99)
+s6 = Student("Faily", 0)
 
 #2
+students = [s1, s2, s3, s4, s5, s6]
 
 #3
-
-#4
+for index, student in enumerate(students, start=1):
+    print(f"[{index}] {student.name} - {student.score}")
 
 #5
+for student in students:
+    print(f"{student.name}: {student.get_status()}")
 
 #6
+at_least_70_score_students = [student for student in students if student.score >= 70]
+#Proof by printing attributes; no point in printing just the object and getting no human language value from it.
+for student in at_least_70_score_students:
+    print(f"{student.name} - {student.score}")
 
 #Part E
 
