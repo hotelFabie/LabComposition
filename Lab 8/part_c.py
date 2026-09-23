edb2eb1e0d33a7accc6d77d794b2b05d356b8a17
hotelFabie <fabianwikman@gmail.com>
2026-09-23 13:17:49 +0200

    lab 8, part c should be done. last question should not be more demanding than this?

--- Lab 8/part_c.py
+++ Lab 8/part_c.py
@@ -2,19 +2,22 @@
 
 #1
 class Account:
-    def __init__(self, owner, balance):
+    def __init__(self, owner : str, balance : float):
         self.owner = owner
         self.balance = balance
 
 #2 and #3
 class SavingsAccount(Account):
-    def __init__(self, owner, balance, interest_rate):
+    def __init__(self, owner : str, balance : float, interest_rate : float):
         super().__init__(owner, balance)
         self.interest_rate = interest_rate
 
 
 #4
-
+sa1 = SavingsAccount("James", 500, 4.5)
+sa2 = SavingsAccount("Annica", 800, 3.5)
+print(f"Acc. 1: {sa1.owner}, balance: {sa1.balance}, interest rate: {sa1.interest_rate}")
+print(f"Acc. 1: {sa2.owner}, balance: {sa2.balance}, interest rate: {sa2.interest_rate}")
 
 #5
-
+#SavingsAccount is an Account because a savings account is a type of account. 
