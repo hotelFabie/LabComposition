2cd65e6033d78a1c53979305106d9a98363a27a6
hotelFabie <fabianwikman@gmail.com>
2026-09-23 14:36:21 +0200

    lab 8, part g should be done.

--- Lab 8/part_g.py
+++ Lab 8/part_g.py
@@ -1,24 +1,25 @@
 #Part G
 
-#WIP
-
 #1
 class Report:
-    def __init__(self):
-        pass
+    def __init__(self, title : str):
+        self.title = title
 
-    def get_summary():
-        return "<summary wip>"    
+    def get_summary(self):
+        return f"[{self.title}]"    
         
 
 #2
 class SalesReport(Report):
-    def __init__(self):
-        super().__init__()
+    def __init__(self, title : str, sales_number : int):
+        super().__init__(title)
+
+        self.sales_number = sales_number
 
     #3
-    def get_summary():
-        return super().get_summary() + "<more stuff>"
+    def get_summary(self):
+        return super().get_summary() + f" {self.sales_number} units have been sold."
 
 #4
-sales_report = SalesReport()
+sales_report = SalesReport("FISH EQUIPMENT", 5000)
+print(sales_report.get_summary())
