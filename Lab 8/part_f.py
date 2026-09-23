9ec3ecde56f41693751c05224c76e9404d4d291f
hotelFabie <fabianwikman@gmail.com>
2026-09-23 14:22:40 +0200

    lab 8 part f should be done, but the last question was a bit weird.

--- Lab 8/part_f.py
+++ Lab 8/part_f.py
@@ -1,3 +1,39 @@
 #Part F
 
-#WIP
+#1
+class Notification:
+    def __init__(self):
+        pass
+
+    def send(self) -> str:
+        return "Notification sent."
+
+#2
+class EmailNotification(Notification):
+    def __init__(self):
+        super().__init__()
+
+    #3
+    def send(self) -> str:
+        return "Email sent."
+
+class SMSNotification(Notification):
+    def __init__(self):
+        super().__init__()
+
+    #3
+    def send(self) -> str:
+        return "SMS sent."
+
+#4
+notification = Notification()
+email_notification = EmailNotification()
+sms_notification = SMSNotification()
+
+print(notification.send())
+print(email_notification.send())
+print(sms_notification.send())
+
+#5
+#In the first case, the superclass's method is being run, 
+#and in the other two (subclass instances), their own variants of the method are called.
